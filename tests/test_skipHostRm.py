c0c5fd0c838f5de4303f10c233f4f989e297ec40
Michael Roach <beardymcjohnface@gmail.com>
2023-06-29 11:10:24 +0930

    BUG: fix tests add simulated test

--- tests/test_skipHostRm.py
+++ tests/test_skipHostRm.py
@@ -1,55 +1,42 @@
-import pytest
+import os
 import gzip
 import shutil
-from unittest import mock
+import pytest
+import gzip
 
-from koverage.scripts.skipHostRm import gzip_file
-
-
-@pytest.fixture
-def temp_dir(tmpdir):
-    return str(tmpdir)
+from trimnami.scripts.skipHostRm import gzip_file
 
 
 @pytest.fixture
-def mock_shutil_copyfile(monkeypatch):
-    def mock_copyfile(src, dst):
-        return
-
-    monkeypatch.setattr(shutil, "copyfile", mock_copyfile)
+def temp_file(tmp_path):
+    file_path = tmp_path / "test.txt"
+    file_path.write_text("Test content")
+    return str(file_path)
 
 
 @pytest.fixture
-def mock_open(monkeypatch):
-    def mock_open_file(file, mode='r'):
-        return mock.mock_open(file, mode)
-
-    monkeypatch.setattr(gzip, "open", mock_open_file)
-
-
-def test_gzip_file_with_gzipped_input_file(temp_dir, mock_shutil_copyfile):
-    input_file = temp_dir + "/input.gz"
-    output_file = temp_dir + "/output.gz"
-
-    with open(input_file, "w") as f:
-        f.write("test content")
-
-    gzip_file(input_file, output_file)
-
-    assert shutil.copyfile.call_args[0][0] == input_file
-    assert shutil.copyfile.call_args[0][1] == output_file
+def temp_file_gz(tmp_path):
+    file_path = tmp_path / "test.gz"
+    with gzip.open(file_path, 'wb') as f:
+        f.write(b"Test content")
+    return str(file_path)
 
 
-def test_gzip_file_with_non_gzipped_input_file(temp_dir, mock_open):
-    input_file = temp_dir + "/input"
-    output_file = temp_dir + "/output.gz"
+def test_gzip_file(tmp_path, temp_file, temp_file_gz):
+    output_file = str(tmp_path / "output.gz")
 
-    with open(input_file, "w") as f:
-        f.write("test content")
+    gzip_file(temp_file_gz, output_file)
 
-    gzip_file(input_file, output_file)
+    assert os.path.isfile(output_file)
+    assert os.path.getsize(output_file) > 0
+    with gzip.open(output_file, 'rb') as f:
+        content = f.read().decode('utf-8')
+        assert content == "Test content"
 
-    assert gzip.open.call_args[0][0] == output_file
-    assert gzip.open.call_args[0][1] == "wb"
-    assert shutil.copyfileobj.call_args[0][0].name == input_file
-    assert shutil.copyfileobj.call_args[0][1].name == output_file
+    gzip_file(temp_file, output_file)
+
+    assert os.path.isfile(output_file)
+    assert os.path.getsize(output_file) > 0
+    with gzip.open(output_file, 'rb') as f:
+        content = f.read().decode('utf-8')
+        assert content == "Test content"
